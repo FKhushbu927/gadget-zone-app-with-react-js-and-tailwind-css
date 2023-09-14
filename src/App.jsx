e03207c8dba161463f4213c32a8eb8e727789bc8
FKhushbu927 <jannateekhushbu927@gmail.com>
2023-09-14 15:22:15 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='bg-purple-50'>HI</h1>
+    </>
+  )
+}
+
+export default App
